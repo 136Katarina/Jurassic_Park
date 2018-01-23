38a05a0954bcdc7767af9c7a324e76ec112b48c6
136Katarina <k.zemplenyiova@gmail.com>
2018-01-23 22:06:17 +0000

    mvp tests passed

--- park.js
+++ park.js
@@ -1,9 +1,37 @@
 const Park = function(){
-this.enclosure = [];
+  this.enclosure = [];
 
 }
 
 
+Park.prototype.enclosureCount = function(){
+  return this.enclosure.length;
+}
 
+Park.prototype.add = function(dinosaur){
+  return this.enclosure.push(dinosaur);
+}
+
+
+
+
+// Park.prototype.remove = function(type){
+//    var newArr = [];
+//   for (dinosaur of this.enclosure) {
+//     if(dinosaur.type === type){
+//       newArr.push(dinosaur);
+//     }
+//   }
+// }
+
+
+Park.prototype.remove = function(type){
+  this.enclosure = this.enclosure.filter(dinosaur => dinosaur.type != type);
+}
+
+
+Park.prototype.offspringCount = function(i){
+  this.enclosure = this.enclosure.filter(dinosaur => dinosaur.number > i);
+}
 
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -17,4 +17,31 @@ describe('park', function(){
     dinosaur3 = new Dinosaur("Velociraptor", 6);
     dinosaur4 = new Dinosaur("Triceratops", 1);
   });
+
+it('enclosure should start empty',function(){
+  assert.strictEqual(park.enclosureCount(), 0);
+})
+
+it('should add a dinosaur', function(){
+  assert.strictEqual(park.add(),1);
+})
+
+it('should remove a dinosaur', function(){
+  park.add(dinosaur2);
+  park.add(dinosaur3);
+  park.add(dinosaur4);
+  park.remove("Velociraptor");
+  assert.strictEqual(park.enclosure.length, 1 )
+})
+
+
+it('should get dinosaurs with offspring count >2', function(){
+  park.add(dinosaur2);
+  park.add(dinosaur3);
+  park.add(dinosaur4);
+  park.add(dinosaur1);
+  park.offspringCount(2)
+  assert.strictEqual(park.enclosure.length, 3)
+})
+
 })
